97c599afcd88a4c41fee47830e9d2a49360e1dc6
Marko <87998044+RealSlovenianGamer@users.noreply.github.com>
2022-11-01 22:44:59 +0100

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'M&M Produkcija';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Objave';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Vse pravice pridržane. M&M Produkcija© 2022';
 
   return {
     name,
